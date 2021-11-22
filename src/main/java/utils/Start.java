fb97be38b3c92f89dcbfcaaea5f38bb8d6d4264b
Martin Wenzel <s203867@student.dtu.dk>
2021-11-22 10:12:36 +0100

    made start field beginning

---
+++ src/main/java/utils/Start.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Start extends Field{
+    private int fare;
+    public Start(String name, String subtext){
+        super(name, subtext,null, false);
+        fare = 2;
+    }
+
+}
