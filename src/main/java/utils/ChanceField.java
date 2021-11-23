29fe9fd0ba47caca4ff0c25802eb02a8126bb4d6
TheGreenLi0n <s200260@student.dtu.dk>
2021-11-23 16:58:24 +0100

    Added ChanceField

---
+++ src/main/java/utils/ChanceField.java
@@ -0,0 +1,25 @@
+package utils;
+
+public class ChanceField extends Field
+{
+    public ChanceField(String subtext, String owner, boolean isVisiting)
+    {
+        super("ChanceField", subtext,  owner, false);
+    }
+
+
+    @Override
+    public int getRent() {
+        return 0;
+    }
+
+    @Override
+    public void landedOn() {
+
+    }
+
+    @Override
+    public void landedOn(ChanceDeck chanceDeck) {
+        chanceDeck.draw();
+    }
+}
