668ba37efc34f08b678f0bde89c92bad8ad454da
TheGreenLi0n <s200260@student.dtu.dk>
2021-11-26 15:32:31 +0100

    Test of draw started

---
+++ src/main/java/Tests/ChanceDeckTest.java
@@ -0,0 +1,25 @@
+package Tests;
+
+import org.junit.jupiter.api.Test;
+import utils.Board;
+import utils.ChanceDeck;
+import utils.FieldController;
+import utils.PlayerController;
+
+import java.io.FileNotFoundException;
+
+class ChanceDeckTest {
+
+    @Test
+    void testDraw() throws FileNotFoundException {
+        ChanceDeck testChanceDeck = new ChanceDeck();
+        FieldController fieldController = new FieldController();
+        Board board = new Board(24,fieldController.getFields());
+        PlayerController playerController = new PlayerController();
+        playerController.createPLayers(board);
+        for (int i = 0; i > 100; i++){
+            testChanceDeck.draw(playerController,board);
+        }
+    }
+
+}
