24bdfae20df348100b1edaf9f668b805517510d2
Joakim Anker Kruse <joakimkruse@hotmail.com>
2021-11-26 15:43:32 +0100

    Added comments to ChanceDeckTest.

--- src/main/java/Tests/ChanceDeckTest.java
+++ src/main/java/Tests/ChanceDeckTest.java
@@ -10,13 +10,18 @@ import java.io.FileNotFoundException;
 
 class ChanceDeckTest {
 
+    //Tests the draw method in the chance deck class to see if it is possible to draw a 100 cards without error.
     @Test
     void testDraw() throws FileNotFoundException {
+        /*
+        Constructs a new chance deck, field controller, bord and  player controller.
+         */
         ChanceDeck testChanceDeck = new ChanceDeck();
         FieldController fieldController = new FieldController();
         Board board = new Board(24,fieldController.getFields());
         PlayerController playerController = new PlayerController();
         playerController.createPlayers(board);
+        //The for loop draws one hundred cards from the deck.
         for (int i = 0; i > 100; i++){
             testChanceDeck.draw(playerController,board);
         }
